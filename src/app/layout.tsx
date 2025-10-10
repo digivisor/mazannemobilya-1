0869a3765b84a4fa9bb19eae5456be0e9860f0df
digivisor <digivisoragency@gmail.com>
2025-10-10 18:28:48 +0300

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,8 +28,17 @@ export default function RootLayout({
       <head>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/themify-icons@0.1.2/css/themify-icons.css" />
                 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Poppins:wght@500;600;700&display=swap" rel="stylesheet"/>
-
-
+                
+                {/* Google tag (gtag.js) */}
+                <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17641417510"></script>
+                <script dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'AW-17641417510');
+                  `
+                }} />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
